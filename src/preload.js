ad0c5868e52ce68db643f7113c11641308ab1699
liberavia <andre.gregor.herrmann@gmail.com>
2022-01-08 19:37:04 +0100

    Remove coments

--- src/preload.js
+++ src/preload.js
@@ -1,21 +1,27 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
-// Expose ipcRenderer to the client
 contextBridge.exposeInMainWorld('ipcRenderer', {
   send: (channel, data) => {
     let validChannels = [
         'check:file:exists'
-    ] // <-- Array of all ipcRenderer Channels used in the client
+    ] 
     if (validChannels.includes(channel)) {
       ipcRenderer.send(channel, data)
+    }
+  },
+  sendSync: (channel, data) => {
+    let validChannels = [
+        'check:file:exists'
+    ] 
+    if (validChannels.includes(channel)) {
+      ipcRenderer.sendSync(channel, data)
     }
   },
   receive: (channel, func) => {
     let validChannels = [
         'check:file:exists'
-    ] // <-- Array of all ipcMain Channels used in the electron
+    ] 
     if (validChannels.includes(channel)) {
-      // Deliberately strip event as it includes `sender`
       ipcRenderer.on(channel, (event, ...args) => func(...args))
     }
   }
